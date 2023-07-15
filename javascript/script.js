1cdef49c10a51a632250dfe5e08c94b68efb7866
w w <lupsetudor@gmail.com>
2023-07-15 20:49:40 +0300

    dada

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+
+
+
+fetch('https://api.jsonbin.io/v3/qs/64b2d6788e4aa6225ebebc66')
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+        // Process the JSON data
+        console.log(data);
+        // You can access the data properties like data.name, data.age, etc.
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
